ac20c26a7729148ca628cabf9adb5a3a5252f79d
Ross Murray <rossmurray@protonmail.com>
2020-11-21 17:55:41 +1100

    fixup: unquote rm cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ if [[ "$build_schemas" == "true" ]]; then
     hamlet entrance -i mock -p aws -p awstest -p azure -p azuretest invoke-entrance -e schema -u reference -o  "$schemas_output_dir"
     hamlet entrance -i mock -p aws -p awstest -p azure -p azuretest invoke-entrance -e schema -u metaparameter -o "$schemas_output_dir"
     # Remove Generated Contracts
-    rm "${schemas_output_dir}/schema-*-generation-contract.json"
+    rm ${schemas_output_dir}/schema-*-generation-contract.json
 fi
