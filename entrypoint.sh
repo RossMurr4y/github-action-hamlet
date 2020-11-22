ee576bbfb0066625708f6fbaaedbc1fe131516da
Ross Murray <rossmurray@protonmail.com>
2020-11-22 12:14:16 +1100

    feat: move loading of properties file into its own script

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,23 +10,9 @@ build_schemas="$1"
 schemas_output_dir="$2"
 properties_file_path="$3"
 
-# read & export properties file into GITHUB_ENV environment file
-# if its provided or discoverable
-if [[ "$properties_file_path" == "" ]]; then
-    # check conventional properties files
-    for f in pipelines/properties/*.properties; do
-        [ -e "$f" ] && properties_file_path="$f" && break
-    done
-    [ ! -n "$properties_file_path" ] && echo "::debug::No properties file found."
-fi
+# Set the environment variables file
+# Includes search for properties file
+/scripts/set_env $properties_file_path "GITHUB_ENV"
 
-if [[ -n "$properties_file_path" ]]; then
-    if [[ -e "$properties_file_path" ]]; then
-        # export unique variables from properties file to GITHUB_ENV
-        cat $properties_file_path | xargs -I {} sh -c "grep -qxF {} GITHUB_ENV || echo {} >> GITHUB_ENV | echo Updated GITHUB_ENV: {}"
-    else
-        echo "::setFailed::Could not locate a properties file at path: ${properties_file_path}"
-    fi
-fi
-
+# Run Tasks
 [ "$build_schemas" == "true" ] && /scripts/build_schemas "$schemas_output_dir"
